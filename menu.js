862a99bc7a7d44e698e18590cfed887ba8f35443
emdesigner-or <77470968+emdesigner-or@users.noreply.github.com>
2022-09-20 16:13:34 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,10 +1,3 @@
-<script>
-// Heading Loading - Menu options loading
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script>
-    
 var textWrapperr = document.querySelector('.ml14');
 textWrapperr.innerHTML = textWrapperr.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -135,4 +128,3 @@ $('.menu-btn-wr').click(function() {
         counttt=0;
     }
 });
-</script>

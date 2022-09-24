bc33b0a3820b3254b78b2c52fe16b44c6a2a3c50
emdesigner-or <77470968+emdesigner-or@users.noreply.github.com>
2022-09-24 11:23:11 +0300

    Update and rename sc-based.js to scroll-based.js

--- scroll-based.js
+++ scroll-based.js
@@ -40,7 +40,7 @@ ScrollTrigger.create({
     duration: 0.2,
   }),
     onLeaveBack: () => gsap.to('#explore-text', {
-    color: "hsla(0, 0%, 100%, 0.7)",
+    color: "hsla(0, 0%, 0%, 0.7)",
     duration: 0.2,
   }),
   onEnterBack: () => gsap.to('#explore-text', {
